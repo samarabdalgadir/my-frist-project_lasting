61d3ed872edebc486cf20b4f4ef968b93258ef87
اسمك < >
2025-06-27 21:47:09 +0300

    Exercise-week-1-typescript&Reac

---
+++ src/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+// src/hello.ts
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+{
+    console.log(greet("Samar"));
+}
---
+++ src/hello.ts
@@ -0,0 +1,15 @@
+
+// src/hello.ts
+
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+let myMessage: string =string; // ❌ خطأ متعمد
+
+{
+    console.log(greet("Samar"));
+
+}
+
+
+
